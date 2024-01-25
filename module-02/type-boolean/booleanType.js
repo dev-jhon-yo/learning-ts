b3cd1318a0651cec897ae9eab19b19105bb8194a
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-01-25 10:10:25 -0300

    feat: type boolean

---
+++ module-02/type-boolean/booleanType.js
@@ -0,0 +1,14 @@
+"use strict";
+// Example 1
+const completedTask = true;
+const pendingTask = false;
+console.log(completedTask);
+console.log(pendingTask);
+// Example 2
+const completed = false;
+if (!completed) {
+    console.log("🟢 Task completed successfully");
+}
+else {
+    console.log("🟡 To do");
+}
---
+++ module-02/type-boolean/booleanType.ts
@@ -0,0 +1,14 @@
+// Example 1
+const completedTask: boolean = true;
+const pendingTask: boolean = false;
+
+console.log(completedTask);
+console.log(pendingTask);
+// Example 2
+const completed: boolean = false;
+
+if (!completed) {
+  console.log("🟢 Task completed successfully");
+} else {
+  console.log("🟡 To do");
+}
