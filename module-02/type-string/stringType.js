8ee6af0ddedafc0b71b1c92cf478007396f68c21
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-01-25 11:14:08 -0300

    feat: type string

---
+++ module-02/type-string/stringType.js
@@ -0,0 +1,11 @@
+"use strict";
+// Example 1 Single Quotes
+const fullName = 'Jhonata Moura';
+console.log(fullName);
+// Example 2 Double Quotes
+const role = "Game Dev";
+console.log(role);
+// Example 3 Back Ticks
+const platform = "Roblox";
+const welcomeMessage = `Welcome ${fullName} you will be a ${role} on ${platform}!`;
+console.log(welcomeMessage);
---
+++ module-02/type-string/stringType.ts
@@ -0,0 +1,13 @@
+// Example 1 Single Quotes
+const fullName: string = 'Jhonata Moura';
+console.log(fullName);
+
+// Example 2 Double Quotes
+const role: string = "Game Dev";
+console.log(role);
+
+// Example 3 Back Ticks
+const platform: string = "Roblox";
+const welcomeMessage: string = `Welcome ${fullName} you will be a ${role} on ${platform}!`;
+
+console.log(welcomeMessage);
