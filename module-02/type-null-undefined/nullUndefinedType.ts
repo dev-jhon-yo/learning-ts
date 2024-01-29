477ac4d9822d6e0082ac1b4464770167ae862050
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-01-29 11:24:11 -0300

    feat: type null and undefined

---
+++ module-02/type-null-undefined/nullUndefinedType.ts
@@ -0,0 +1,15 @@
+// Example 1 - Null
+const testNull = null;
+
+console.log(testNull);
+console.log(typeof testNull);
+
+// Example 2 - Undefined
+let testUndefined;
+
+console.log(testUndefined);
+console.log(typeof testUndefined);
+
+// Null vs Undefined
+console.log('Example 01:', null == undefined);
+console.log('Example 02:', null === undefined);
